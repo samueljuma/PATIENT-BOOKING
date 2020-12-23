da3e1d9ff9d5ce3d0f84899bc0f87afae70a05cc
SamJay0 <samjay0.254@gmail.com>
2020-12-23 20:54:24 +0300

    mainMenu()

--- src/Booking.java
+++ src/Booking.java
@@ -1,7 +1,17 @@
 public class Booking {
-
-
+    public static void mainMenu() {
+        System.out.print("_________________________________________________"
+                + "\n\n\tROTTEN PARK MEDICAL CENTRE (RMPC)"
+                + "\n_________________________________________________"
+                + "\n\t1.Book an Apointment"
+                + "\n\t2.Cancel an Appointment"
+                + "\n\t3.View a doctor's schedule"
+                + "\n\t4.View a patient's Appointment"
+                + "\n\t5.Exit"
+                + "\n-------------------------------------------------"
+                + "\n\tPlease enter an option: ");
+    }
     public static void main(String[] args) {
-        System.out.printf("Hello");
+
     }
 }
